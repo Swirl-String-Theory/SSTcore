914391d75bb73d54fa907491cf3299e8d2c627d1
Omar Iskandarani <info@omariskandarani.com>
2026-08-13 21:20:44 +0200

    full probes updated

--- include/generated/knot_files_embedded.h
+++ include/generated/knot_files_embedded.h
@@ -1,13 +1,24 @@
 // Auto-generated header - do not edit manually
-#ifndef KNOT_FILES_EMBEDDED_H
 #define KNOT_FILES_EMBEDDED_H
 
+#ifndef KNOT_FILES_EMBEDDED_H
 #include <map>
+#define KNOT_FILES_EMBEDDED_H
+
 #include <string>
 
+#include <map>
 namespace sst {
+#include <string>
+
     std::map<std::string, std::string> get_embedded_knot_files();
+namespace sst {
     std::map<std::string, std::string> get_embedded_ideal_files();
+    std::map<std::string, std::string> get_embedded_knot_files();
+}
+
+    std::map<std::string, std::string> get_embedded_ideal_files();
+#endif // KNOT_FILES_EMBEDDED_H
 }
 
 #endif // KNOT_FILES_EMBEDDED_H
